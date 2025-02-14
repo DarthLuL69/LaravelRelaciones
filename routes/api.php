90d224cf629e1a5e49bed9e071bf737ddd8b15c4
Darthull lol <phancer666@gmail.com>
2025-02-14 17:27:15 +0100

    IMPORTANTE

--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,5 @@ use App\Http\Controllers\UserController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::get('/users/{id}/profile', [UserController::class, 'showProfile']);
 Route::get('/users/{id}/posts', [UserController::class, 'showPosts']);
